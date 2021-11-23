8376b7e913fc02dff5b83e2710196e70d91603d4
PGuti <pierre.j.p.gutierrez@gmail.com>
2021-11-23 11:05:00 +0100

    adds first version of working api

--- app/main.py
+++ app/main.py
@@ -1,15 +1,55 @@
-from typing import Optional
+from fastapi import FastAPI, UploadFile, File
 
-from fastapi import FastAPI
+# for now we only support NASNEtLarge.
+# Supporting more models could be an improvement to be done.
+from tensorflow.keras.applications.nasnet import preprocess_input
+from tensorflow.keras.applications import NASNetLarge
+
+from utils import read_imagefile, process_and_predict_image
 
 app = FastAPI()
+
+# load model
+model = NASNetLarge(
+    input_shape=None,
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    pooling=None,
+    classes=1000,
+)
 
 
 @app.get("/")
 def read_root():
+    """Dummy default fast api
+
+    Returns:
+            Hello world
+    """
+
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict/")
+async def prediction(file: UploadFile = File(...)):
+    """Predict what is on an image
+
+    Args:
+
+    Returns:
+                    The probabilities associated with the inference
+    """
+
+    # check that the file can be processed
+    supported_extensions = ("jpg", "jpeg", "png")
+    extension = file.filename.split(".")[-1]
+    if extension not in supported_extensions:
+        return "Unsuported extension of file. Please pick one in {}".format(
+            supported_extensions
+        )
+    image = read_imagefile(await file.read())
+    prediction = process_and_predict_image(
+        image, preprocessing_func=preprocess_input, model=model
+    )
+    return prediction
---
+++ app/utils.py
@@ -0,0 +1,54 @@
+from PIL import Image
+from io import BytesIO
+import numpy as np
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
+
+
+def read_imagefile(file):
+    """Read an uploaded image
+
+    Args:
+        file:
+
+    Returns:
+        a PIL image
+    """
+    image = Image.open(BytesIO(file))
+    return image
+
+
+def get_model_input_shape(model):
+    """Get the resolution of image the model was trained with
+
+    Args:
+        model: a tf.keras model
+
+    Returns:
+        a tuple containing image resolution (H,W,C)
+    """
+    return tuple(list(model.input.shape)[1:])
+
+
+def formatted_prediction(prediction):
+    return [{"id": p[0], "class": p[1], "probability": float(p[2])} for p in prediction]
+
+
+def process_and_predict_image(image, preprocessing_func, model, nb_decode=5):
+    """Compute predictions on an image
+
+    Args:
+            image: a PIL Image
+
+    Returns:
+
+    """
+    # preprocess
+    shape = get_model_input_shape(model)
+    image = image.resize(shape[:2])
+    image = np.asarray(image)
+    preprocessed_image = preprocessing_func(image)
+    # predict
+    predictions = model.predict(np.array([preprocessed_image]))
+    # decode prediction
+    predictions = decode_predictions(predictions, nb_decode)[0]
+    return formatted_prediction(predictions)
